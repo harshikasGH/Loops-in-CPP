d11b69773d40e59541af8be5b51ef424522f7e33
Harshika Kamwal <harshikakamwal@gmail.com>
2025-09-29 19:05:40 +0530

    Floyd's Series.cpp

--- Floyd's Series.cpp
+++ Floyd's Series.cpp
@@ -1,5 +1,5 @@
-// Aditya Jadhav
-// 24070123005
+// Harshika Kamwal
+// 25070123508
 
 #include <iostream>
 using namespace std;
@@ -31,4 +31,5 @@ int main() {
 // 2 3 
 // 4 5 6 
 // 7 8 9 10 
+
 // 11 12 13 14 15
