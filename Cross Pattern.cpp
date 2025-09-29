d08c07a9da824566f8195e584e5fd3c2ed5dc9d5
Harshika Kamwal <harshikakamwal@gmail.com>
2025-09-29 19:03:49 +0530

    Cross Pattern.cpp

--- Cross Pattern.cpp
+++ Cross Pattern.cpp
@@ -1,5 +1,5 @@
-// Aditya Jadhav
-// 24070123005
+// Harshika Kamwal
+// 25070123508
 
 #include <iostream>
 using namespace std;
@@ -28,4 +28,5 @@ int main() {
 //   +   +   
 //     +     
 //   +   +   
+
 // +       +
